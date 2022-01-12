5ff6db0f24cc4d80432b859e35d0c3c07ed86d7b
Alexander Novik <alexander.novik@expcapital.com>
2022-01-12 23:24:22 +0300

    add effects

---
+++ sketch_fire/sketch_fire.ino
@@ -0,0 +1,34 @@
+#include <FastLED.h>
+
+#define EFFECT_BUTTON_PIN 9
+
+boolean lastEffectButton = LOW; 
+boolean currentEffectButton = LOW;
+
+void setup() {
+  // Buttons
+  pinMode(EFFECT_BUTTON_PIN, INPUT);
+  setupEffects();
+}
+
+void loop() {
+   listenEffectButton();
+   showEffect();
+}
+
+void listenEffectButton() {
+  currentEffectButton = debounce(lastEffectButton);
+  if (lastEffectButton == LOW && currentEffectButton == HIGH) {
+     incrementMode();
+  }
+  lastEffectButton = currentEffectButton;
+}
+
+boolean debounce(boolean last) {
+  boolean current = digitalRead(EFFECT_BUTTON_PIN);
+  if (last != current) {
+    delay(5);
+    current = digitalRead(EFFECT_BUTTON_PIN);
+    return current;
+  }
+}
